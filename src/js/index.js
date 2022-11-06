e9f891986a9672aed58f130443fdfed7f3c9d535
J.M. Robin <johnmichaelrobin@gmail.com>
2022-11-06 21:19:59 +0800

    Add notification when submitting valid email address

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,11 @@
 const form = document.querySelector(".form");
 const email = document.querySelector(".form__input");
-const errorText = document.querySelector(".form__error");
+const errorText = document.querySelector(".form__text--error");
+const successText= document.querySelector(".form__text--success");
 
 form.addEventListener("submit", function (event) {
     event.preventDefault();
-    console.log("Submit");
+
     let pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
     if (email.value === "" || !email.value.match(pattern)) {
@@ -12,12 +13,14 @@ form.addEventListener("submit", function (event) {
         email.classList.add("border-red");
     } else {
         email.value = "";
+        successText.classList.add("visible");
     }
 })
 
 function removeClass() {
     email.classList.remove("border-red");
     errorText.classList.remove("visible");
+    successText.classList.remove("visible");
 }
 
 email.addEventListener("click", function (event) {
